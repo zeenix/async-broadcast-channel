c26c133d1166a8467f55cd16a7a53dadb032e7bb
Zeeshan Ali <zeeshanak@gnome.org>
2021-04-01 19:51:45 +0200

    Some fixes to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! An MPMC broadcast library.
 //!
-//! While provides a nice and simple multi-producer-multi-consumer channel, this library provides
-//! broadcasting feature, where every message sent on the channel is received by every reeceiver.
-//! Since the ownership of the data is transfered, the data is cloned for each receiver and hence
-//! [`Clone`] trait is required on the type of the data being transmitted.
+//! While [`async_channel`] provides a nice and simple multi-producer multi-consumer channel, this
+//! library provides broadcasting feature, where every message sent on the channel is received by
+//! every receiver. Since the ownership of the data is transfered, the data is cloned for each
+//! receiver and hence [`Clone`] trait is required on the type of the data being transmitted.
 //!
 //! ### Examples
 //!
@@ -22,7 +22,7 @@
 //! assert_eq!(receiver2.try_recv(), Ok(2));
 //! ```
 //!
-//! [`async_channel`]: https://crates.io/crates/async-channel 
+//! [`async_channel`]: https://crates.io/crates/async-channel
 
 use std::sync::{Arc, RwLock};
 use async_channel::{RecvError, SendError, TryRecvError, TrySendError};
